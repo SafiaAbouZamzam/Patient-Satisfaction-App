19fde4fdc59d794361c241aa8ff5bc255fba4d77
Safia <safiaabouzamzam@gmail.com>
2018-07-24 12:19:14 -0700

    Initial Storyboard Satisfaction App
    Initial repository for satisfaction with patient care.

--- Satisfaction App/ViewController.swift
+++ Satisfaction App/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var yesOutlet: UIButton!
+    @IBOutlet weak var noOutlet: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+
+
+    @IBAction func yesAction(_ sender: UIButton) {
+        print("Yes")
+    }
+    
+    @IBAction func noAction(_ sender: UIButton) {
+        print("No")
+    }
+    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
